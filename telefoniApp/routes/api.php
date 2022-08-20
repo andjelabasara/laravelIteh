f506c898b9c02a0d781e22a78286269e9a46c2b1
andjelabasara <andjela.basara@gmail.com>
2022-08-20 15:48:13 +0200

    dodata autentifikacija korisnika

--- telefoniApp/routes/api.php
+++ telefoniApp/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TelefonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,14 +15,22 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/registrujse',[AuthController::class,'register']);
+Route::post('/ulogujse',[AuthController::class,'login']);
+
+
+
+
 Route::get('/telefoni',[TelefonController::class,'index']);
 Route::get('/telefoni/{id}',[TelefonController::class,'show']);
 
-Route::delete('/telefoni/{id}',[TelefonController::class,'destroy']);
 
-Route::post('/telefoni',[TelefonController::class,'store']);
-Route::put('/telefoni/{id}',[TelefonController::class,'update']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::post('/odjava',[AuthController::class,'logout']);
+    Route::delete('/telefoni/{id}',[TelefonController::class,'destroy']);
+
+    Route::post('/telefoni',[TelefonController::class,'store']);
+    Route::put('/telefoni/{id}',[TelefonController::class,'update']);
 });
